bee3fbea758eacb37708a0dfb957e3ba26a76900
nishamasanta <157500324+nishamasanta@users.noreply.github.com>
2024-09-10 20:32:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "nisha@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+ accountEmail ="hc@hc.com"
+ accountPassword ="212121"
+accountCity = "Kolkata" 
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs not browser
+
+console.log(3+3);
+
+console.log("Nisha")
+
+let name ="Nisha"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2^ 53
+//bigint
+//string => ""
+//boolean => true/false
+//null  => standalone value/ representation of empty value
+// undefined => do not define value
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Nisha");
+console.log(typeof age);
+console.log(typeof null);
