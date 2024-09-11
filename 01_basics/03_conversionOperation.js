a5bd0132cadadc77030688aff2e86cf85cbfae68
nishamasanta <157500324+nishamasanta@users.noreply.github.com>
2024-09-11 20:39:05 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc"=> NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 =>true; 0=>false
 // ""=> false
@@ -23,5 +23,41 @@ console.log(booleanIsLoggedIn);
 let someNumber= 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+// *********************  Operations ***************************
+
+let value = 3
+let negValue = -value;
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 =" Nisha"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"") //these are not work in practical
+
+let num1, num2,num3
+
+num1 = num2 =num3 = 2+2 //main things in code that code must be readable and clear
+
+let gameCounter = 100
+gameCounter++; //in prefix values are increment at first and in the case of postfix values are incerement after uses
+console.log(gameCounter);
