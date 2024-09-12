7ac9fbb8d4dfe4a52ea953ca3fc84f41f81f47ad
nishamasanta <157500324+nishamasanta@users.noreply.github.com>
2024-09-12 18:32:24 +0000

    Comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log("02" > 1);
+// console.log("2" > 1);
+
+
+// These comparision are very confusing so we all avoid it
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+
+console.log("2" === 2);
